fb1e64ea3e0039a2d9ebff3eecc122fd09bc9c97
VictorRubinec <vrubinec@gmail.com>
2024-11-05 18:22:40 -0300

    [FEATURE] - adicionando segmento de envio dos dados para o google sheets (autor do código: Marco)

--- aws_data_generation/filter_client.py
+++ aws_data_generation/filter_client.py
@@ -1,10 +1,16 @@
 import boto3
 import json
 from datetime import datetime
+import gspread
+from google.oauth2.service_account import Credentials
+import os
 
 class Filter:
     
     def __init__(self):
+        
+        
+        
         self.s3 = boto3.client('s3')
         self.buckets = {
             "trusted": "tote-trusted",
@@ -16,6 +22,18 @@ class Filter:
             "client": []
         }
         self.file_path = '/home/ubuntu/horus/last_archive_client.txt'
+        
+        scope = ["https://spreadsheets.google.com/feeds", 'https://www.googleapis.com/auth/spreadsheets',
+             "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/drive"]
+        path_credential = '/credentials/sheets/'
+        credential = self.get_credential(path_credential)
+        creds = Credentials.from_service_account_file(credential, scopes=scope)
+        self.client = gspread.authorize(creds)
+        self.sheet_key = os.getenv('SHEET_KEY')
+        
+    def get_credential(self, path_credential):
+        with open(path_credential, 'r') as f:
+            return json.load(f)
         
     def get_objects(self):
         for bucket in self.buckets:
@@ -39,6 +57,12 @@ class Filter:
                 )
                 
                 self.final_object = key_object
+                
+                print(f"Arquivo {key_object} processado e transferido para o bucket client.")
+                
+                self.send_data_sheets(dados_clients)
+                
+                print("Dados enviados com sucesso para o Google Sheets!")
     
     def save_last_file(self):
         if self.buckets_objects['trusted']:
@@ -58,7 +82,7 @@ class Filter:
         for setor in dados_trusted["setores"]:
             setor_client = {
                 "setor": setor["setor"],
-                "paineis": []  # Modificado para incluir 'paineis'
+                "paineis": []
             }
 
             for painel in setor["paineis"]:
@@ -81,11 +105,61 @@ class Filter:
                     if horario or razao_temp or energia_gerada or energia_esperada or tensao:
                         painel_client["dados"].append(dado)
 
-                setor_client["paineis"].append(painel_client)  # Adiciona painel ao setor
+                setor_client["paineis"].append(painel_client)
 
             dados["setores"].append(setor_client)
 
         return dados
+    
+    def send_data_sheets(self, data_client):
+        sheet = self.client.open_by_key(self.sheet_key)
+        worksheet = sheet.get_worksheet(0)
+        
+        headers = ["Cliente", "Setor", "Painel", "Data/Hora", "Obstrução", "Luminosidade", 
+                    "Temperatura Externa", "Temperatura Interna", "Tensão", 
+                    "Energia Gerada", "Energia Esperada", "Eficiência", "Céu", "Umidade",
+                    "Direcionamento", "Inclinação"]
+        
+        worksheet.update('A1', [headers])
+        
+        try:
+            with open(data_client, 'r', encoding='utf-8') as file:
+                dados = json.load(file)
+        except Exception as e:
+            print(f"Erro ao carregar o arquivo JSON: {e}")
+            return
+        
+        cliente = dados['cliente']
+        
+        for setor_data in dados['setores']:
+            setor = setor_data['setor']
+
+            for painel_data in setor_data['paineis']:
+                painel = painel_data['painel']
+
+                for dado in painel_data['dados']:
+                    linha = [
+                        cliente,                    
+                        setor,                      
+                        painel,                     
+                        dado['data_hora'],          
+                        dado['obstrucao'],          
+                        dado['luminosidade'],       
+                        dado['temperatura_externa'], 
+                        dado['temperatura_interna'],
+                        dado['tensao'],             
+                        dado['energia_gerada'],     
+                        dado['energia_esperada'],   
+                        dado['eficiencia'],         
+                        dado['ceu'],                
+                        dado['umidade'],                
+                        dado['direcionamento'],     
+                        dado['inclinacao']          
+                    ]
+
+                    worksheet.append_row(linha)
+
+                print("Dados enviados com sucesso!")
     
     def run(self):
         self.get_objects()
